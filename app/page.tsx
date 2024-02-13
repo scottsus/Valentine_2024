eff9b0febc4c6707ed9ccdb824076b226a9cdc99
scott susanto <susantoscott@gmail.com>
2024-02-13 02:02:53 -0800

    web + mobile ready, first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,127 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import { useState } from 'react';
+import styles from './page.module.css';
+
+type YesButtonProps = {
+  fontSize: string;
+};
 
 export default function Home() {
+  const [saidYes, setSaidYes] = useState(false);
+
+  const [yesButtonSize, setYesButtonSize] = useState<YesButtonProps>({
+    fontSize: '20px',
+  });
+  const updateYesButtonSize = (yesButtonSize: YesButtonProps) => {
+    const fontSizeNumber = parseInt(yesButtonSize.fontSize);
+    const newFontSize = `${fontSizeNumber + 10}px`;
+
+    return { fontSize: newFontSize };
+  };
+  const handleYesClick = () => {
+    setSaidYes(true);
+  };
+
+  const [noMessageIndex, setNoMessageIndex] = useState(0);
+  const noButtonMessages = [
+    'No',
+    'Wait what',
+    'Why not',
+    'Are you sure',
+    "I don't want the best bf in the world",
+    "If u don't say yes I'm gonna start showing embarassing pics of u 🤭",
+    'MY TEST SUBJECT!!! U want another one?',
+    'How do u like the choke',
+    'Hehe I love eggs',
+    'Why so hangry',
+    'Sassy 👱‍♀️',
+    'Ok no more joking',
+    'Ok fr now',
+    'Oi stop',
+    'Press yes gdi',
+    'Excuse me what',
+    'JUST PRESS YES',
+    'Dont be mean ☹️',
+    "I'm sad now ☹️☹️",
+    'Babe why',
+    'U leave me no choice',
+  ];
+  const handleNoClick = () => {
+    setNoMessageIndex((noMessageIndex) => noMessageIndex + 1);
+    setYesButtonSize((yesButtonSize) => updateYesButtonSize(yesButtonSize));
+  };
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {saidYes ? (
+        <>
+          <h1 className={styles.heading}>YEAAAAAAAAAAHHHHHH!!! 💃</h1>
+          <img
+            src="gifs/celebrate.gif"
+            className={`${styles.gif} ${styles.celebrate}`}
+          />
+          <p className={styles.subtitle}>🧺 excited for our date today!!!</p>
+        </>
+      ) : (
+        <>
+          {noMessageIndex > 5 && (
+            <div className={styles.embarassingPicturesContainer}>
+              {noMessageIndex > 5 && (
+                <img
+                  src="embarassing/lab.jpeg"
+                  className={`${styles.embarassingPictures}`}
+                />
+              )}
+              {noMessageIndex > 6 && (
+                <img
+                  src="embarassing/choke.jpeg"
+                  className={`${styles.embarassingPictures}`}
+                />
+              )}
+              {noMessageIndex > 7 && (
+                <img
+                  src="embarassing/egg.jpeg"
+                  className={`${styles.embarassingPictures}`}
+                />
+              )}
+              {noMessageIndex > 8 && (
+                <img
+                  src="embarassing/hangry.jpeg"
+                  className={`${styles.embarassingPictures}`}
+                />
+              )}
+              {noMessageIndex > 9 && (
+                <img
+                  src="embarassing/moody.jpeg"
+                  className={`${styles.embarassingPictures}`}
+                />
+              )}
+            </div>
+          )}
+          <h1 className={styles.heading}>
+            now that midterms are over... will you be my valentine 🤭
+          </h1>
+          <img src="gifs/ask.gif" className={`${styles.gif} ${styles.ask}`} />
+          <div className={styles.buttonContainer}>
+            <button
+              onClick={handleYesClick}
+              className={`${styles.button} ${styles.yes}`}
+              style={yesButtonSize}
+            >
+              Yes
+            </button>
+            {noMessageIndex < noButtonMessages.length && (
+              <button
+                onClick={handleNoClick}
+                className={`${styles.button} ${styles.no}`}
+              >
+                {noButtonMessages[noMessageIndex]}
+              </button>
+            )}
+          </div>
+        </>
+      )}
     </main>
   );
 }
